a0305c660da0daea2a39571b13ba7cf0ccbf6d91
sahil devnani <sahil.devnani@uleth.ca>
2023-03-17 12:37:02 -0600

    commit

--- prefixsum.cpp
+++ prefixsum.cpp
@@ -1,47 +1,87 @@
 #include <iostream>
-#include <cstdlib>
+#include <vector>
+#include <cmath>
 #include <cilk/cilk.h>
-#include <cilk/cilk_api.h>
+#include "ctimer.h"
 
 using namespace std;
 
-void prefix_sum(double* arr, int n, int p) {
-    int block_size = n / p;
-    double* block_sums = new double[p];
-    cilk_for(int i = 0; i < p; i++) {
-        double sum = 0;
-        for (int j = i * block_size; j < (i + 1) * block_size; j++) {
-            sum += arr[j];
-            arr[j] = sum;
+// Variables used in the functions
+int first, last;
+
+// Split input array into p chunks and return vector of sub-arrays
+vector<vector<double>>mydividedarray(int n, int p, double* myarray) {
+  int myarraysize = ceil((double)n/p);
+  std::vector<std::vector<double>>myvector(p);
+  // Here we split up the array and return the vector of subarrays
+  cilk_scope {
+     for (int i = 0; i < p; i++) {
+        first = i * myarraysize;
+        last = i + 1 * myarraysize;
+        myvector[i].resize(last - first);
+        for (int j = first; j < last; j++) {
+            myvector[i][j - first] = myarray[j];
         }
-        block_sums[i] = sum;
     }
-    if (p > 1) {
-        prefix_sum(block_sums, p, p / 2);
-        cilk_for(int i = 1; i < p; i++) {
-            int block_start = i * block_size;
-            arr[block_start] += block_sums[i - 1];
-        }
-    }
-    delete[] block_sums;
+  }
+  return myvector;
 }
 
-int main(int argc, char* argv[]) {
-    int n = 1000000;
-    if (argc > 1) {
-        n = atoi(argv[1]);
+// Perform prefix sum on a subarray by adding the last element of the previous subarray to the first element of the current subarray
+void prefix_sum(vector<double>& sub_array, double lastnumber) {
+  cilk_scope {
+    for (int i = 0; i < sub_array.size(); i++) {
+        sub_array[i] = sub_array[i] + lastnumber;
+        lastnumber = lastnumber +  sub_array[i];
     }
-    double* arr = new double[n];
-    double M = 100.0;
-    srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        arr[i] = (double)rand() / RAND_MAX * 2 * M - M;
-    }
-    prefix_sum(arr, n, __cilkrts_get_nworkers());
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    delete[] arr;
-    return 0;
+  }
 }
+
+int main (int argc, char *argv[]) {
+    //long n = pow(10, 6);
+    //if (argc > 1) {
+      //  n = atol(argv[1]);
+   // }
+
+
+  double arr[] = {1,2,3,4,5,6,7,8,9,10};
+    
+
+  double lastnumber = 0;
+  int p = 5;
+  int n = 10;
+  
+  ctimer_t t;
+  ctimer_start(&t);
+
+  vector<vector<double>> sub_arrays = mydividedarray(n, p, arr);
+
+  // Perform prefix sum on each subarray
+     
+    for (int i = 0; i < p; ++i) {
+        prefix_sum(sub_arrays[i], lastnumber);
+        if (i < p - 1) {
+        lastnumber = sub_arrays[i][sub_arrays[i].size() -1];
+        }
+    }
+
+    ctimer_stop(&t);
+    ctimer_measure(&t);
+
+  // Combine sub-arrays into one output array
+  std::vector<double> output_array;
+  for (int i = 0; i < p; ++i) {
+    output_array.insert(output_array.end(), sub_arrays[i].begin(), sub_arrays[i].end());
+  }
+
+  // Print output array
+  std::cout << "Output array: ";
+  for (int i = 0; i < output_array.size(); ++i) {
+    std::cout << output_array[i] << " ";
+  }
+     ctimer_print(t, "Prefix");
+
+  return 0;
+}
+// SO we have taken a array, than split it into many subarrays, after than we have computed the prefix sum also we return to the user the final prefix sum
+// we just neeed to see what robert is looking for in terms of the output.
