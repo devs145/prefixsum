7f0f496d7c59b919441da5407701d1390bb48cac
sahil devnani <sahil.devnani@uleth.ca>
2023-03-20 20:15:25 -0600

    commit

--- prefixsum.cpp
+++ prefixsum.cpp
@@ -16,48 +16,43 @@ The following function splits up the input array.
 */
 vector<vector<double>>mydividedarray(int n, int p, double* myarray) {
   int myarraysize = ceil((double)n/p);
-  vector<std::vector<double>>myvector(p);
-  cilk_scope {
-    for (int i = 0; i < p; i++) {
-      first = i * myarraysize;
-      last = i + 1 * myarraysize;
-      myvector[i].resize(last - first);
-      for (int j = first; j < last; j++) {
-        myvector[i][j - first] = myarray[j];
-      }
+  vector<vector<double>>myvector(p);
+
+  cilk_for (int i = 0; i < p; i++) {
+    first = i * myarraysize;
+    last = i + 1 * myarraysize;
+    myvector[i].resize(last - first);
+    cilk_for (int j = first; j < last; j++) {
+      myvector[i][j - first] = myarray[j];
     }
   }
   return myvector;
 }
 
-/* 
-Perform prefix sum on the subarrays by adding the last element 
+/*
+Perform prefix sum on the subarrays by adding the last element
 of the previous subarray to the first element of the current subarray.
 @param subarray, and lastnumber are being passed in.
 */
 void prefixsum(vector<double>&mysubarray, double lastnumber) {
-  cilk_scope {
-    for (int i = 0; i < mysubarray.size(); i++) {
-      mysubarray[i] = mysubarray[i] + lastnumber;
-      lastnumber = lastnumber +  mysubarray[i];
-    }
+  cilk_for (int i = 0; i < mysubarray.size(); i++) {
+    mysubarray[i] = mysubarray[i] + lastnumber;
+    lastnumber = lastnumber +  mysubarray[i];
   }
 }
 
-
 int main (int argc, char *argv[]) {
   double lastnumber = 0;
-  int p = 10;
-  int n = 5;
-    
+  long p = 10;
+  long n = 5;
+
   if (argc > 1) {
     p = atol(argv[1]);
   }
   double arr[p];
 
   for (int i = 0; i < p; i++) {
-    //arr[i] = rand() - RAND_MAX / 2 ; // 
-    arr[i] = rand() % 200 - 100; 
+    arr[i] = rand() % 200 - 100;
   }
 
   cout << "Prior Computation: " << endl;
@@ -71,7 +66,7 @@ int main (int argc, char *argv[]) {
   vector<vector<double>> mysubarray = mydividedarray(n, p, arr);
 
   // Perform prefix sum on each subarray
-     
+
   for (int i = 0; i < p; i++) {
     prefixsum(mysubarray[i], lastnumber);
     if (i < p - 1) {
@@ -96,5 +91,7 @@ int main (int argc, char *argv[]) {
 
   cout << endl;
   ctimer_print(t, "Prefix");
+  // Verification if it is correct or not do one for if number of entries are greater than 10 as the results are too large to print out on console
+
   return 0;
 }
